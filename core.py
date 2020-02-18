26898f3ee27ba91f5a3ac594453d0398b886ba80
fubar2 <ross.lazarus@gmail.com>
2020-02-18 14:11:13 +1100

    removed temp debugging print

--- core.py
+++ core.py
@@ -195,11 +195,20 @@ class GraphManager(object):
 		self.logger = logging.getLogger("graphmanager")
 		self.logger.setLevel(logging.DEBUG)
 		self.args = args
-		privatestarts = ['10.','192.168.','255.255.255.255','0.0.0.0']
+		privatestarts = ['10.','192.168.','255.255.255.255','0.0.0.0','224.0.0','224.0.1','224.1','224.2','232.'.'233.','234.','239.']
 		more = ["172.%d" % i for i in range(16,32)]
 		privatestarts += more
 		more =  ["100.%d" % i for i in range(64,128)]
 		privatestarts += more
+		more =  ["224.0.%d" % i for i in range(2,256)]
+		privatestarts += more
+		more =  ["224.%d" % i for i in range(252,256)]
+		privatestarts += more
+		more =  ["%d" % i for i in range(225,231)]
+		privatestarts += more
+		more =  ["%d" % i for i in range(225,231)]
+		privatestarts += more
+		
 		self.privates = privatestarts
 		# if any ([x.startswith[y] for y in privates]): # is private
 		try:
